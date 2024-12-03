fc426f8c9577351559a2487c9b351f740bc0f2e3
DioneKx <mjhonnyvieira@gmail.com>
2024-12-03 17:42:31 -0300

    Jhonny: Re: Test

---
+++ Test/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 32;
+        double y = 10.35784;
+
+        System.out.println("Hello World!");
+        System.out.println(x);
+        System.out.println(y);
+    }
+
+}
--- test_program/src/hello_world/Main.java
+++
@@ -1,9 +0,0 @@
-package hello_world;
-
-public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
-
-}

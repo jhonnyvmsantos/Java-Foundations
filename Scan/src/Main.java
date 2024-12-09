ad69a164f28836b0e8b8c163a7c6eaa63781aebc
DioneKx <mjhonnyvieira@gmail.com>
2024-12-09 20:47:23 -0300

    Jhonny: Scanner-Line

--- Scan/src/Main.java
+++ Scan/src/Main.java
@@ -10,11 +10,16 @@ public class Main {
 
         String s1, s2, s3; //Criação de multiplas variaveis sem valor definido
 
+        s2 = sc.nextLine(); //nextLine() torna possivel ler um dado string de toda uma linha
+        s3 = sc.nextLine();
+
+        System.out.printf("Frase digitada (P/Linha): %s | %s\n", s2, s3);
+
         int a;
         double b;
         char c;
 
-        //Utilizando metodos diferentess de nextLine(), pode-se ler dados (diferentes tipos incluso) numa mesma linha, separadas por espaço
+        //Utilizando metodos diferentes de nextLine(), pode-se ler dados (diferentes tipos incluso) numa mesma linha, separadas por espaço
         //A forma mais normal é ler esses dados linha por linha ao apertar Enter após a digitação do dado
         s1 = sc.next(); //Começar a leitura de dados do tipo string
         c = sc.next().charAt(2); //Começar a leitura de dados do tipo char uma determinada posição de uma cadeia de caracteres

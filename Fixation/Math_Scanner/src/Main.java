81c135f2719f38973c745eb5c5536a6135cf2dc1
DioneKx <mjhonnyvieira@gmail.com>
2024-12-11 20:17:05 -0300

    Jhonny: Fixation-Fix

--- Fixation/Math_Scanner/src/Main.java
+++ Fixation/Math_Scanner/src/Main.java
@@ -6,18 +6,39 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        //Calculo de áreas (Três valores na entrada)
-        double e, f, g;
-        e = sc.nextDouble();
-        f = sc.nextDouble();
-        g = sc.nextDouble();
-        System.out.printf("Triangulo: %.3f\n", e * g / 2);
-        System.out.printf("Circulo: %.3f\n", 3.14159 * Math.pow(g, 2));
-        System.out.printf("Trapezio: %.3f\n", ((e + f) / 2) * g);
-        System.out.printf("Quadrado: %.3f\n", Math.pow(f, 2));
-        System.out.printf("Retangulo: %.3f\n", e * f);
+        System.out.println("Retire os comentários no código para começar os calculos.");
 
-        //Calculo com "peça" (Seis valores na entrada - Três p/cada peça)
+        //Soma dos números (Dois valores na entrada)
+//        int x, y;
+//        x = sc.nextInt();
+//        y = sc.nextInt();
+//
+//        System.out.printf("Soma dos Valores: %d\n", x + y);
+
+
+        //Área de um circulo (Um valor na entrada)
+//        double area = 3.14159 * Math.pow(sc.nextDouble(), 2);
+//        System.out.printf("Área do circulo: %.4f\n", area);
+
+
+//        //Diferença entre dois pares de números (Quatro valores na entrada)
+//        int a, b, c, d;
+//        a = sc.nextInt();
+//        b = sc.nextInt();;
+//        c = sc.nextInt();
+//        d = sc.nextInt();
+//        System.out.printf("Diferença entre os pares: %d", a * b - c * d);
+
+
+        //Calculo do salario de um funcionário (Três valores na entrada)
+//        int number = sc.nextInt();
+//        double hours, valor;
+//        hours = sc.nextDouble();
+//        valor = sc.nextDouble();
+//        System.out.printf("O funcionário N°%d recebe o valor de %.2f p/hora trabalhada, totalizando um salário de %.2f após %.2f horas.\n", number, valor, valor * hours, hours);
+
+
+        //Calculo do valor total de duas "peças" (Seis valores na entrada - Três valores p/cada peça)
 //        int code1, qtd1, code2, qtd2;
 //        double val1, val2;
 //        code1 = sc.nextInt();
@@ -28,30 +49,18 @@ public class Main {
 //        val2 = sc.nextDouble();
 //        System.out.printf("Valor total a pagar pelas peças N°%d e N°%d: %.2f\n", code1, code2, qtd1 * val1 + qtd2 * val2);
 
-        //Calculo de salario de um funcionário (Três valores na entrada)
-//        int number = sc.nextInt();
-//        double hours, valor;
-//        hours = sc.nextDouble();
-//        valor = sc.nextDouble();
-//        System.out.printf("O funcionário N°%d recebe o valor de %.2f p/hora trabalhada, totalizando um salário de %.2f após %.2f horas.\n", number, valor, valor * hours, hours);
 
-//        //Diferença entre dois pares de números (Quatro valores na entrada)
-//        int a, b, c, d;
-//        a = sc.nextInt();
-//        b = sc.nextInt();;
-//        c = sc.nextInt();
-//        d = sc.nextInt();
-//        System.out.printf("Diferença entre os pares: %d", a * b - c * d);
+        //Calculo de áreas (Três valores na entrada)
+//        double e, f, g;
+//        e = sc.nextDouble();
+//        f = sc.nextDouble();
+//        g = sc.nextDouble();
+//        System.out.printf("Triangulo: %.3f\n", e * g / 2);
+//        System.out.printf("Circulo: %.3f\n", 3.14159 * Math.pow(g, 2));
+//        System.out.printf("Trapezio: %.3f\n", ((e + f) / 2) * g);
+//        System.out.printf("Quadrado: %.3f\n", Math.pow(f, 2));
+//        System.out.printf("Retangulo: %.3f\n", e * f);
 
-        //Área de um circulo (Um valor na entrada)
-//        double area = 3.14159 * Math.pow(sc.nextDouble(), 2);
-//        System.out.printf("Área do circulo: %.4f\n", area);
-
-        //Soma dos números (Dois valores na entrada)
-//        int x, y;
-//        x = sc.nextInt();
-//        y = sc.nextInt();
-//
-//        System.out.printf("Soma dos Valores: %d\n", x + y);
+        sc.close();
     }
 }

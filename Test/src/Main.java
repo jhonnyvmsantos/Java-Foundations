33d3976a61b4c6f43a70006f086bdeef49e2f842
DioneKx <mjhonnyvieira@gmail.com>
2024-12-12 20:09:05 -0300

    Jhonny: Conditions-Optional

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -9,23 +9,29 @@ public class Main {
 
         String text = "Random";
         int x = 12;
-        double y = 122.6453;
         int z = sc.nextInt();
+        double y = (z >= 0) ? 122.6453 : 6453.122; //Expressão de condição ternária
 
-        if (z >= 0) {
+        if (z > 0) {
             System.out.print("Hello "); //Print sem quebra de linha
             System.out.println("World!"); //Print com quebra de linha
-        } else {
+        } else if (z == 0) {
             System.out.println("Hello && World!");
+        } else {
+            System.out.println("World! && Hello");
         }
 
-        if (z == 10) {
-            System.out.printf("Int: %d\n", x); //Print com formatação ("\n" add quebra de linha)
-        } else if (!(z == 5)) {
-            System.out.printf("Double: %.2f number%n", y); //"%n" (também) add quebra de linha
-        } else {
-            //"%s" (String), "%d" (int), "%f" (flutuante)
-            System.out.printf("%s Number is %d and %.2f", text, x, y); //Texto formatado com variaveis incluidas
+        switch (z) {
+            case 10:
+                System.out.printf("Int: %d\n", x); //Print com formatação ("\n" add quebra de linha)
+                break;
+            case 5:
+                System.out.printf("Double: %.2f number%n", y); //"%n" (também) add quebra de linha
+                break;
+            default:
+                //If sem chaves a seguir
+                if (z <= 0) System.out.printf("%s Number is %d and %.4f", text, x, y); //Texto formatado com variaveis incluidas
+                break;
         }
 
         sc.close();

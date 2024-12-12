3dae9c101501e76373ee535e6233a1d2f124e90d
DioneKx <mjhonnyvieira@gmail.com>
2024-12-12 19:56:20 -0300

    Jhonny: Condition-Fixation

---
+++ Fixation/Condition/src/Main.java
@@ -0,0 +1,61 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Retire os comentários no código para começar os calculos.");
+
+
+        //Leitura do valor do número (Um valor na entrada)
+//        int x = sc.nextInt();
+//
+//        if (x >= 0) {
+//            System.out.println("Número positivo.");
+//        } else {
+//            System.out.println("Número negativo.");
+//        }
+
+
+        //Separação de número par/ímpar (Um valor na entrada)
+//        int y = sc.nextInt();
+//
+//        if ((y % 2) == 0) {
+//            System.out.println("Número par.");
+//        } else {
+//            System.out.println("Número ímpar.");
+//        }
+
+
+        //Identificação de números multiplos (Dois valores na entrada)
+//        int a, b;
+//        a = sc.nextInt();
+//        b = sc.nextInt();
+//
+//        if ((b % a) == 0 || (a % b) == 0) {
+//            System.out.println("São multiplos.");
+//        } else {
+//            System.out.println("Não são multiplos.");
+//        }
+
+
+        //Calculo de tempo de um jogo (Dois valores na entrada)
+//        int c, d;
+//        c = sc.nextInt();
+//        d = sc.nextInt();
+//
+//        if (c < d) {
+//            System.out.printf("O jogo durou %d hora(s)", Math.abs(c - d));
+//        }
+//        else {
+//            System.out.printf("O jogo durou %d hora(s)", Math.abs(24 - c + d));
+//        }
+
+        sc.close();
+
+    }
+
+}

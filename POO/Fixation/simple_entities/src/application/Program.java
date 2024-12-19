2017aa8dcfee12663de4499a58693a4456a6c343
DioneKx <mjhonnyvieira@gmail.com>
2024-12-19 12:54:06 -0300

    Jhonny: Fix

--- POO/Fixation/simple_entities/src/application/Program.java
+++ POO/Fixation/simple_entities/src/application/Program.java
@@ -11,22 +11,22 @@ public class Program {
 
     //Para executar algum dos exercicios de fixação, retire os comentários, deixando APENAS 1 não comentado (Em funcionamento)
 
-    public static void main(String[] args) {
-
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
-        Rectangle rectangle = new Rectangle();
-
-        System.out.print("Enter the rectangle width and height: ");
-        rectangle.width = sc.nextDouble();
-        rectangle.height = sc.nextDouble();
-
-        System.out.println("Area = " + rectangle.area());
-        System.out.println("Perimeter = " + rectangle.perimeter());
-        System.out.println("Diagonal = " + rectangle.diagonal());
-
-        sc.close();
-    }
+//    public static void main(String[] args) {
+//
+//        Locale.setDefault(Locale.US);
+//        Scanner sc = new Scanner(System.in);
+//        Rectangle rectangle = new Rectangle();
+//
+//        System.out.print("Enter the rectangle width and height: ");
+//        rectangle.width = sc.nextDouble();
+//        rectangle.height = sc.nextDouble();
+//
+//        System.out.println("Area = " + rectangle.area());
+//        System.out.println("Perimeter = " + rectangle.perimeter());
+//        System.out.println("Diagonal = " + rectangle.diagonal());
+//
+//        sc.close();
+//    }
 
 
 //    public static void main(String[] args) {
@@ -57,36 +57,39 @@ public class Program {
 //    }
 
 
-//    public static void main(String[] args) {
-//
-//        Locale.setDefault(Locale.US);
-//        Scanner sc = new Scanner(System.in);
-//        Student student = new Student();
-//
-//        boolean result;
-//
-//        System.out.println("Enter the student data:");
-//
-//        System.out.print("Name: ");
-//        student.name = sc.nextLine();
-//
-//        System.out.print("1° Tri. grade: ");
-//        student.grade[0] = sc.nextDouble();
-//
-//        System.out.print("2° Tri. grade: ");
-//        student.grade[1] = sc.nextDouble();
-//
-//        System.out.print("3° Tri. grade: ");
-//        student.grade[2] = sc.nextDouble();
-//
-//        System.out.println("Final Grade: " + student.finalGrade());
-//
-//        result = student.finalResult();
-//        System.out.println(result ? "Pass" : "Failed");
-//
-//        if (!result) System.out.printf("Missing %.2f points", student.missingGrade());
-//
-//        sc.close();
-//    }
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Student student = new Student();
+
+        int count = 0;
+        boolean result;
+
+        System.out.println("Enter the student data:");
+
+        System.out.print("Name: ");
+        student.name = sc.nextLine();
+
+        while (count < 3) {
+            System.out.printf("%d° Tri. grade (%s max): ", count + 1, (count == 0) ? "30" : "35");
+            student.grade[count] = sc.nextDouble();
+
+            if (count == 0) {
+                if (student.grade[count] <= 30) count++;
+            } else {
+                if (student.grade[count] <= 35) count++;
+            }
+        }
+
+        System.out.println("Final Grade: " + student.finalGrade());
+
+        result = student.finalResult();
+        System.out.println(result ? "Pass" : "Failed");
+
+        if (!result) System.out.printf("Missing %.2f points", student.missingGrade());
+
+        sc.close();
+    }
 
 }

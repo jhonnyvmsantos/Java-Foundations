939baa79cad023e147845b63413e008498d6e3a4
DioneKx <mjhonnyvieira@gmail.com>
2024-12-19 10:15:46 -0300

    Jhonny: Poo

--- POO/poo_test/src/application/Program.java
+++ POO/poo_test/src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import entities.Triangle;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -9,29 +11,28 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        double xA, xB, xC, yA, yB, yC;
-        double pX, pY, areaX, areaY;
+        Triangle x, y;
+        x = new Triangle();
+        y = new Triangle();
 
         System.out.println("Enter the measures (3) of triangle X:");
-        xA = sc.nextDouble();
-        xB = sc.nextDouble();
-        xC = sc.nextDouble();
-
-        pX = (xA + xB + xC) / 2;
-        areaX = Math.sqrt(pX * (pX - xA) * (pX - xB) * (pX - xC));
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
+        x.p = (x.a + x.b + x.c) / 2;
+        x.area = Math.sqrt(x.p * (x.p - x.a) * (x.p - x.b) * (x.p - x.c));
 
         System.out.println("Enter the measures (3) of triangle Y:");
-        yA = sc.nextDouble();
-        yB = sc.nextDouble();
-        yC = sc.nextDouble();
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
+        y.p = (y.a + y.b + y.c) / 2;
+        y.area = Math.sqrt(y.p * (y.p - y.a) * (y.p - y.b) * (y.p - y.c));
 
-        pY = (yA + yB + yC) / 2;
-        areaY = Math.sqrt(pY * (pY - yA) * (pY - yB) * (pY - yC));
+        System.out.printf("Triangle X area: %.4f\n", x.area);
+        System.out.printf("Triangle Y area: %.4f\n", y.area);
 
-        System.out.printf("Triangle X area: %.4f\n", areaX);
-        System.out.printf("Triangle Y area: %.4f\n", areaY);
-
-        System.out.printf("Larger area: %s", areaX > areaY ? "X" : "Y");
+        System.out.printf("Larger area: %s", x.area > y.area ? "X" : "Y");
 
         sc.close();
     }

e5bc8ddaf0acfc0b697b581300f5319633ad0a96
DioneKx <mjhonnyvieira@gmail.com>
2024-12-19 10:02:21 -0300

    Jhonny: No-Poo

---
+++ POO/poo_test/src/application/Program.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double xA, xB, xC, yA, yB, yC;
+        double pX, pY, areaX, areaY;
+
+        System.out.println("Enter the measures (3) of triangle X:");
+        xA = sc.nextDouble();
+        xB = sc.nextDouble();
+        xC = sc.nextDouble();
+
+        pX = (xA + xB + xC) / 2;
+        areaX = Math.sqrt(pX * (pX - xA) * (pX - xB) * (pX - xC));
+
+        System.out.println("Enter the measures (3) of triangle Y:");
+        yA = sc.nextDouble();
+        yB = sc.nextDouble();
+        yC = sc.nextDouble();
+
+        pY = (yA + yB + yC) / 2;
+        areaY = Math.sqrt(pY * (pY - yA) * (pY - yB) * (pY - yC));
+
+        System.out.printf("Triangle X area: %.4f\n", areaX);
+        System.out.printf("Triangle Y area: %.4f\n", areaY);
+
+        System.out.printf("Larger area: %s", areaX > areaY ? "X" : "Y");
+
+        sc.close();
+    }
+}

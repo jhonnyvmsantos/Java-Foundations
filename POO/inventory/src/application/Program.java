53d8b07fb5496a19ae8bded010751a8a01ccf243
DioneKx <mjhonnyvieira@gmail.com>
2025-01-03 17:36:51 -0300

    Jhonny: Constructor

--- POO/inventory/src/application/Program.java
+++ POO/inventory/src/application/Program.java
@@ -12,18 +12,19 @@ public class Program {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        Product product = new Product();
 
         System.out.println("Enter product data:");
 
         System.out.print("Name: ");
-        product.name = sc.nextLine();
+        String name = sc.nextLine();
 
         System.out.print("Price: ");
-        product.price = sc.nextDouble();
+        double price = sc.nextDouble();
 
         System.out.print("Quantity in stock: ");
-        product.quantity = sc.nextInt();
+        int quantity = sc.nextInt();
+
+        Product product = new Product(name, price, quantity); //Enviando valores para o constructor
 
 //        System.out.println(product.toString()); //Transformando o objeto em String
 
--- POO/inventory/src/entities/Product.java
+++ POO/inventory/src/entities/Product.java
@@ -1,9 +1,17 @@
 package entities;
 
 public class Product {
-    public String name;
-    public double price;
-    public int quantity;
+
+    public String name; //Valor padrão: null
+    public double price; //Valor padrão: 0.0
+    public int quantity; //Valor padrão: 0
+
+    //"Constructor" é uma operação especial da classe, executada no momento da instanciação do objeto
+    public Product(String name, double price, int quantity) { //Definindo valores a serem, obrigatoriamente, alocados
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
 
     public double totalValueInStock() {
         return price * quantity;
@@ -11,14 +19,14 @@ public class Product {
 
     public void addProduct(int quantity) { //Criando um método sem retorno
         //Ao utilizar "this", faz-se referencia ao atributo da classe, evitando uma ambiguidade por nomes iguais
-        this.quantity += quantity;
+        this.quantity += quantity; //A palavra "this" é uma referencia ao proprio objeto/classe
     }
 
     public void removeProduct(int qtd) {
         quantity -= qtd;
     }
 
-    public String toString() { //Sobrepondo a operação (padrão) toString da classe Object para o que eu quiser
+    public String toString() { //Sobrepondo a operação (padrão) "toString" da classe Object para o que eu quiser
         //O método "String.format" serve para definir um formato de exibição para uma variavel
         return name + ", $ " + String.format("%.2f", price) + ", " + quantity + " units. Total: $ " + String.format("%.2f", totalValueInStock());
     }

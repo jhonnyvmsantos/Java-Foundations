f68372222aea687dc3443a1c14cf33b76f4e7737
DioneKx <mjhonnyvieira@gmail.com>
2025-01-08 19:12:30 -0300

    Jhonny: Geral-Tests

---
+++ POO/geral_test/src/application/Program.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.util.Locale;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        int x = 102;
+
+//        Object obj = x; //"Boxing" (Conversão de um obj tipo primitivo valor para tipo referencial)
+        Integer obj = null; //"Wrapper Class" (Tipo refencial para um tipo primitivo. Ex: Boolean, Char, Number...)
+        obj = x;
+        //Wrapper Class são classes, sendo, normalmente, usadas no lugar de tipos primitivos (Podem aceitar valores nulos)
+
+        //"Unboxing" (Conversão do tipo referêncial pro tipo valor)
+//        int y = (int) obj - 3; //Casting pro tipo INT
+        int y = obj - 3;
+
+        System.out.println(x + " | " + y);
+    }
+}

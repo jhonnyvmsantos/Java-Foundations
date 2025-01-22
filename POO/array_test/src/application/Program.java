5acde1e9fe5a3dd99edf29310f7c08dfdc5b3554
DioneKx <mjhonnyvieira@gmail.com>
2025-01-22 18:07:07 -0300

    Jhonny: Matriz-2D

---
+++ POO/array_test/src/application/Program.java
@@ -0,0 +1,42 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Set the size of the array (Ex: 0): ");
+        int n = sc.nextInt();
+
+        int[][] arr = new int[n][n];
+        int nCount = 0;
+
+        //Método com "for padrão" para percorrer uma matriz
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter three (3) integers: ");
+
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+
+                if (arr[i][j] < 0) {
+                    nCount++;
+                }
+            }
+        }
+
+        System.out.println("Main diagonal: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i][i] + " ");
+        }
+
+        System.out.printf("Quantity of negative numbers: %d", nCount);
+
+        sc.close();
+    }
+
+}

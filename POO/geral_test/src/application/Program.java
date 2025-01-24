c57288cd2f7630b4168b0ff50cb812ac8749b17c
DioneKx <mjhonnyvieira@gmail.com>
2025-01-24 17:47:37 -0300

    Jhonny: Data-Hora

--- POO/geral_test/src/application/Program.java
+++ POO/geral_test/src/application/Program.java
@@ -1,27 +1,67 @@
 package application;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
+import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        //"DateTimeFormatter" serve para definir padrões customizados de data/hora
+        DateTimeFormatter dFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy"); //Data customizada
+        DateTimeFormatter dtFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"); //Data/Hora customizada
+
+        LocalDate dLocal = LocalDate.now(); //Traz apenas a data local
+        LocalDateTime dtLocal = LocalDateTime.now(); //Traz a data e hora local
+        Instant dtGlobal = Instant.now(); //Traz a data e hora de londres (Global - UTC)
+
+        //Método para conseguir um obj "date" a partir de dados isolados
+//        dLocal = LocalDate.of(2022, 7, 10);
+
+        //Converter texto (Formato ISO 8601) para um objeto "date"...
+//        dLocal = LocalDate.parse("2022-01-11");
+//        dtLocal = LocalDateTime.parse("2024-01-24T18:00:00");
+//        dtGlobal = Instant.parse("2020-04-02T12:00:10Z");
+        //Transformando do horario de São Paulo (-3:00) para o de Londres
+//        dtGlobal = Instant.parse("2020-04-02T12:00:10-03:00");
+        //Convetendo para obj "date" atraves de um formato customizado
+//        dLocal = LocalDate.parse("11/02/2012", dFormat);
+        dtLocal = LocalDateTime.parse("11/02/2012 21:09", dtFormat);
+
+        //Objeto está sendo automáticamente transformado em String pelo método "toString"...
+        System.out.println("Data Local: " + dLocal);
+        System.out.println("Data/Hora Local: " + dtLocal);
+        System.out.println("Data/Hora Londres: " + dtGlobal);
 
         int x = 102;
 
 //        Object obj = x; //"Boxing" (Conversão de um obj tipo primitivo valor para tipo referencial)
-        Integer obj; //"Wrapper Class" (Tipo refencial para um tipo primitivo. Ex: Boolean, Char, Number...)
-        //Wrapper Class são classes, sendo, normalmente, usadas no lugar de tipos primitivos (Iniciam "null")
-        obj = x;
+        Integer obj = x; //"Wrapper Class" Tipo Primitivo -> Tipo refencial. (Ex: Boolean, Char, Number...)
+        //Wrapper Class são classes, sendo, normalmente, usadas no lugar de tipos primitivos
 
-        //"Unboxing" (Conversão do tipo referêncial pro tipo valor)
-//        int y = (int) obj - 3; //Casting pro tipo INT
-        int y = obj - 3;
+        //"Unboxing" (Conversão do tipo referêncial pro tipo valor):
+//        int y = (int) obj - 3; //Casting pro tipo INT (Utilizando Boxing)
+        int y = obj - 3; //(Utilizando Wrapper Class ou Boxing)
 
         System.out.println(x + " | " + y);
+
+        System.out.print("Executar outra função do projeto (y/n)? ");
+        char verification = sc.next().charAt(0);
+        if (verification == 'y') {
+            alter_main();
+        }
+
+        sc.close();
+    }
+
+    public static void alter_main() {
 
         //"List" é um tipo "Interface", necessitando de uma classe que a implementa
         //Interface é um tipo que define a especificação das operações (Não pode ser instanciada)

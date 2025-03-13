1c7f750ba24776cccae4122d3bf426ae34edeee0
DioneKx <mjhonnyvieira@gmail.com>
2025-03-12 21:32:50 -0300

    Jhonny: Stack-Trace

--- POO/exception_test/src/application/Program.java
+++ POO/exception_test/src/application/Program.java
@@ -7,8 +7,23 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
 
+        first();
+    }
+
+    public static void first() {
+        System.out.println("---- First Start ----");
+
+        second();
+
+        System.out.println("---- First End ----");
+    }
+
+    public static void second() {
+
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("---- Second Start ----");
 
         try { //Executa o codigo em seu interior
             String[] vect = sc.nextLine().split(" ");
@@ -18,11 +33,13 @@ public class Program {
         } catch (ArrayIndexOutOfBoundsException e) { //Tal erro se refere a uma posição inexistente no array
             //Captura um possivel erro e executa o codigo em seu interior
             System.out.println("Invalid position!");
+
+//            e.printStackTrace(); //Metodo utilizado para vizualizar todo o "caminho" até a exceção ocorrer
         } catch (InputMismatchException e) { //Tal erro se refere a uma entrada de dados invalida
             System.out.println("Input Error!");
         }
 
-        System.out.println("Hello, World!");
+        System.out.println("---- Second End ----");
 
         sc.close();
     }

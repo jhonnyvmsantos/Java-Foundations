47108af9386967fef6d1368c3b6f1019059deb95
DioneKx <mjhonnyvieira@gmail.com>
2025-03-14 19:02:23 -0300

    Jhonny: Exception-Bad

--- POO/hotel_reservation/src/application/Program.java
+++ POO/hotel_reservation/src/application/Program.java
@@ -38,7 +38,6 @@ public class Program {
         if (!checkOut.after(checkIn)) {
             System.out.println("Error in reservation: Check-out date nust be after check-in date.");
         } else {
-            Date now = new Date();
             Reservation reservation = new Reservation(number, checkIn, checkOut);
             System.out.println("Reservation: " + reservation);
 
@@ -50,15 +49,13 @@ public class Program {
             System.out.print("Check-out date (dd/MM/yyyy): ");
             checkOut = sdf.parse(sc.next());
 
-            if (checkIn.before(now) || checkOut.before(now)) {
-                System.out.println("Error in reservation: Reservation dates for update must be future dates.");
-            } else if (!checkOut.after(checkIn)) {
-                System.out.println("Error in reservation: Check-out date nust be after check-in date.");
+            String error = reservation.updateDates(checkIn, checkOut);
+
+            if (error != null) {
+                System.out.println("Error in reservation: " + error);
             } else {
-                reservation.updateDates(checkIn, checkOut);
                 System.out.println("Reservation: " + reservation);
             }
-
         }
 
         sc.close();
